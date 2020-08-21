4eada623140a497cb598e7fd3223b6fd3da0be45
relody <1442096232@qq.com>
2020-08-21 18:43:39 +0800

    Update PaypalConstants.java

--- src/main/java/com/binghuang/paypal_sdk/constants/PaypalConstants.java
+++ src/main/java/com/binghuang/paypal_sdk/constants/PaypalConstants.java
@@ -3,12 +3,12 @@ package com.binghuang.paypal_sdk.constants;
 public class PaypalConstants {
 
     // paypal账号
-    private final static String payPalAccount = "1442096232@qq.com";
+    private final static String payPalAccount = "xxxx.com";
 
     // paypal appID对应客户端id
-    private final static String clientID = "Acs5uYdrurLmVP7fLh28Fu4kDKoDs-o0ZEQXXxaLZh6TrBqjRux2BleN1aGSYnVyoldWi0MD9jIXo7Be";
+    private final static String clientID = "xxxx";
 
     // paypal当前客户端连接秘钥
-    private final static String secret = "ELlCZPmZqJoxrn-nxJPkkK5nJlzyaCKalPGvnSvZ-mDyxiPB_s8hJHu1Sql1ZRaDVgNUBkyEusXqQv9Z";
+    private final static String secret = "xxxx";
 
 }
